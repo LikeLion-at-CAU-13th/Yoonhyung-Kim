bd2ae1deb4e74ac48864dd99faf7f34d559ada31
YoonisKim <runheng1209@gmail.com>
2025-04-06 13:57:08 +0900

    feat: 카테고리별로 게시물  조회 및 게시물별로 댓글 조회 기능 추가

--- likelion13/comments/urls.py
+++ likelion13/comments/urls.py
@@ -3,4 +3,5 @@ from comments.views import *
 
 urlpatterns = [
     path('<int:id>', get_comment_detail), # 추가
+    path('post/<int:post_id>/',comment_list, name='comment_list')
 ]
--- likelion13/posts/urls.py
+++ likelion13/posts/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-    path('<int:id>', get_post_detail), # 추가
+	#path("", hello_world, name="hello_world"),
+    #path("page", index, name="my-page"),
+    #path('<int:id>', get_post_detail),
+ 	
+    path('', post_list, name="post_list"),
+    path('<int:post_id>/', post_detail, name='post_detail'), # Post 단일 조회
+    path('category/<int:category_id>/',post_list_by_category, name='post_list_by_category'),
 ]
