90df9e30df0c87e44fde7443355030f4a6359228
YoonisKim <runheng1209@gmail.com>
2025-05-04 01:02:51 +0900

    feat: comment api 수정, comment id를 통한 조회 및 삭제 기능 추가

--- likelion13/comments/views.py
+++ likelion13/comments/views.py
@@ -4,10 +4,18 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 from .models import *
 
+from .serializers import CommentSerializer
+
+# APIView를 사용하기 위해 import
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
 # Create your views here.
 
 @require_http_methods(["GET"])
-def get_comment_detail(reqeust, id):
+def get_comment_detail(request, id):
     comment = get_object_or_404(Comment, pk=id)
     comment_detail_json = {
         "id" : comment.id,
@@ -23,7 +31,7 @@ def get_comment_detail(reqeust, id):
 #게시물 별로 댓글 조회
 @require_http_methods(["GET"])
 def comment_list(request, post_id):
-    post = get_object_or_404(Comment,pk=post_id)
+    post = get_object_or_404(Post,pk=post_id)
     comments = Comment.objects.filter(post=post)
     comment_json_all = []
 
@@ -41,3 +49,21 @@ def comment_list(request, post_id):
             'message': '게시글 목록 조회 성공',
             'data': comment_json_all
         })
+
+class CommentList(APIView):
+    def get(self, request, post_id):
+        comments = Comment.objects.filter(id=post_id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    
+class CommentDetail(APIView):
+    def get(self, request, post_id,comment_id):
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(Comment, pk = comment_id, post=post)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    def delete(self,request,post_id,comment_id):
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(Comment, pk = comment_id, post=post)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
