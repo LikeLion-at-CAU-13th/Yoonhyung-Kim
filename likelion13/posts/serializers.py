a68f2239960341e2982d32f95481a17030c82038
YoonisKim <runheng1209@gmail.com>
2025-06-25 02:26:22 +0900

    feat: week12 file uploaded

--- likelion13/posts/serializers.py
+++ likelion13/posts/serializers.py
@@ -2,6 +2,7 @@
 
 from rest_framework import serializers
 from .models import Post
+from .models import Image
 
 class PostSerializer(serializers.ModelSerializer):
 
@@ -11,3 +12,8 @@ class PostSerializer(serializers.ModelSerializer):
 		# 모델에서 어떤 필드를 가져올지
 		# 전부 가져오고 싶을 때
     fields = "__all__"
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = "__all__"
--- likelion13/posts/urls.py
+++ likelion13/posts/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     # path('logtest/', test_log_view),
 
     path('', PostList.as_view()), # post 전체 조회
-    path('<int:post_id>/', PostDetail.as_view()) # post 개별 조회
+    path('<int:post_id>/', PostDetail.as_view()), # post 개별 조회
+    path('upload/', ImageUploadView.as_view(), name='image-upload'),
 ]
